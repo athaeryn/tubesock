96a57985aa925ef46b4390b9449b76b1749b57a2
Mike Anderson <hello@mrmikea.com>
2014-12-10 18:49:57 -0600

    Add shebang

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,6 +1,6 @@
+#!/usr/bin/env node
+
 var TubeSock = require("../"),
     minimist = require("minimist");
 
-TubeSock(
-  minimist(process.argv.slice(2))
-);
+TubeSock(minimist(process.argv.slice(2)));
